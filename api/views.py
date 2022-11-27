ad5735f5cdeefd67d6c311a707237b1b25b0e18d
Olusola Adekunle <olusolaadek@hotmail.com>
2022-11-27 22:39:03 +0100

    upgrade view to allow create

--- api/views.py
+++ api/views.py
@@ -6,9 +6,13 @@ from todo.models import Todo
 # Create your views here.
 
 
-class TodoList(generics.ListAPIView):
+class TodoListCreate(generics.ListCreateAPIView):
     serializer_class = TodoSerializer
 
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        # serializer holds a django model
+        serializer.save(user=self.request.user)
