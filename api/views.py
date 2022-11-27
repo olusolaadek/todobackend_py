a4733224191aae0ffaff62ef37482f785cb21a2b
Olusola Adekunle <olusolaadek@hotmail.com>
2022-11-27 23:35:21 +0100

    implement complete toggle view and serializer

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 from rest_framework import generics, permissions
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
 
 # Create your views here.
@@ -27,3 +27,16 @@ class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
         user = self.request.user
         # user can only update, delete own posts
         return Todo.objects.filter(user=user)
+
+
+class TodoToggleComplete(generics.UpdateAPIView):
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+    def perform_update(self, serializer):
+        serializer.instance.completed = not (serializer.instance.completed)
+        serializer.save()
