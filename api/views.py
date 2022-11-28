2699db639d102160f974fcd0a13ad962d70761ac
Olusola Adekunle <olusolaadek@hotmail.com>
2022-11-28 22:23:07 +0100

    add login functionality

--- api/views.py
+++ api/views.py
@@ -2,6 +2,14 @@
 from rest_framework import generics, permissions
 from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
+# import auth related packages
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -40,3 +48,36 @@ class TodoToggleComplete(generics.UpdateAPIView):
     def perform_update(self, serializer):
         serializer.instance.completed = not (serializer.instance.completed)
         serializer.save()
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)  # data is a dictionary
+            user = User.objects.create_user(
+                username=data['username'], password=data['password'])
+            user.save()
+
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse({'error': 'username taken choose another username'}, status=400)
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(
+            request, username=data['username'],
+            password=data['password'])
+
+    if user is None:
+        return JsonResponse({'error': 'Unable to login. Check username and password'}, status=400)
+    else:  # return user token
+        try:
+            token = token.objects.get_or_create(user=user)
+        except Exception:  # if token not in db, create a new one
+            token = Token.objects.create(user=user)
+    return JsonResponse({'token': str(token)}, status=201)
